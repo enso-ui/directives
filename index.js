905f3931356e5f485002d010f7c4b0e531ddef67
mishu <mishu@xtelecom.ro>
2019-03-06 12:56:15 +0200

    small refactor

--- index.js
+++ index.js
@@ -3,4 +3,6 @@ import focus from './src/focus';
 import hljs from './src/hljs';
 import selectOnFocus from './src/selectOnFocus';
 
-export { clickOutside, focus, hljs, selectOnFocus };
+export {
+    clickOutside, focus, hljs, selectOnFocus,
+};
